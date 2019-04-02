1a23b643bc0f3d2c0e513d3a51074f1482f9cab5
banix8 <darwindelvo235@gmail.com>
2019-04-02 07:34:23 -0700

    Add files via upload
    Updated OrderModule

--- application/views/contents/shop/home.php
+++ application/views/contents/shop/home.php
@@ -5,7 +5,7 @@
 		<div class="container-fluid">
                 <?php foreach ($latest_products as $latest) { ?>
                     <div class="panel-body well" style=" float:left;padding:20px;margin:10px;">
-                        <a href="<?= base_url('Shop/details?product='.$latest->product_id)?>" ><img style="height:150px" src="<?php echo base_url('uploads/'.$latest->product_image)?>" alt="" /></a>
+                        <a href="<?= base_url('Shop/details?product='.$latest->product_id)?>" ><img style="width:250px;height:250px" src="<?php echo base_url('uploads/'.$latest->product_image)?>" alt="" /></a>
                         <h4><?php echo $latest->product_name?></h4>
                         <p><?php echo $latest->short_desc?></p>
                         <p><span class="price"><?php echo $this->cart->format_number($latest->sell_price) ?> PHP</span></p>
@@ -21,7 +21,7 @@
 		<div class="container-fluid">
                 <?php foreach ($products as $single_products) { ?>
                     <div class="panel-body well" style=" float:left;padding:20px;margin:10px;">
-                        <a  href="<?= base_url('Shop/details?product='.$single_products->product_id)?>" ><img style="height:150px" src="<?php echo base_url('uploads/'.$single_products->product_image)?>" alt="" /></a>
+                        <a  href="<?= base_url('Shop/details?product='.$single_products->product_id)?>" ><img style="width:250px;height:250px" src="<?php echo base_url('uploads/'.$single_products->product_image)?>" alt="" /></a>
                         <h4><?php echo $single_products->product_name?></h4>
                         <p><?php echo $single_products->short_desc?></p>
                         <p><span class="price"><?php echo $this->cart->format_number($single_products->sell_price) ?> PHP</span></p>
